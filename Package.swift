a37a45bfb59d03e3e57138f9f9e377d3aa8e79f0
Larry Damman <ldamman@nomadpool.com>
2017-08-20 02:01:01 -0400

    test

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "ConsistencyManager-iOS",
 	targets: [
-		Target(name: "ConsistencyManager-iOS", dependencies: []),
+		Target(name: "ConsistencyManager", dependencies: []),
 	],
 	dependencies: [
 	],
